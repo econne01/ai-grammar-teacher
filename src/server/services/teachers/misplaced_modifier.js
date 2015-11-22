9ccefa2eb08ab0022ff85df20affa2f7b162f797
Eric Connelly <eric.connelly08@gmail.com>
2015-11-22 18:27:21 -0500

    WIP almost have misplaced modifier working

--- src/server/services/teachers/misplaced_modifier.js
+++ src/server/services/teachers/misplaced_modifier.js
@@ -15,8 +15,15 @@ var MisplacedModifierTeacher = BaseTeacher.extend({
         // Any 2 prepositional phrases in a row
         /IN,[\w]+,IN,[\w]+/,
         // Only, Just, Nearly, Almost (Adjectives acting on verbs?) descriptors
-        new RegExp('.*' + NOUN_REGEX + ',JJ,VB.*')
+        new RegExp(NOUN_REGEX + ',JJ,VB')
     ],
+    modifierSentenceRules : {
+        // Any 2 prepositional phrases in a row
+        'swap_2_prepositions': {
+            'sentenceStructure': /(IN,[\w]+),(IN,[\w]+)/,
+            'newStructure': '$2,$1' 
+        }
+    },
 
     /**
      * Return list of possible edits to the text that could misplace a modifier
@@ -28,9 +35,9 @@ var MisplacedModifierTeacher = BaseTeacher.extend({
         var errors = [];
         var sentences = nlp.pos(inputText).sentences;
         _.each(sentences, function(sentence) {
-            if (this._hasModifierSentenceStructure(sentence)) {
-                var editItem = new EditItem(0, 10, 'TESTTEST -', 'misplaced-modifier');
-                errors.push(editItem);
+            var newSentence = this._alterModifierSentence(sentence);
+            if (newSentence) {
+                errors.push(newSentence);
             };
         }, this);
         return errors;
@@ -42,19 +49,55 @@ var MisplacedModifierTeacher = BaseTeacher.extend({
      * @param {nlp.Sentence}
      * @returns {Boolean}
      */
-    _hasModifierSentenceStructure : function (sentence) {
+    _alterModifierSentence : function (sentence) {
         var isMatchingStructure = false,
             structure = sentence.tags().join(),
             tokens = this.parseToTokens(sentence.text());
-        _.find(this.modifierSentenceStructures, function(modifierStructure) {
+        _.find(this.modifierSentenceRules, function(modifierRule) {
+            var modifierStructure = modifierRule.sentenceStructure;
             if (modifierStructure.test(structure)) {
                 isMatchingStructure = true;
+                var match = structure.match(modifierStructure);
                 var matchStart = structure.search(modifierStructure);
-                var startToken = structure.slice(0, matchStart).split(',').length;
+                // @todo - this looks like a bug in case matchStart is 0...
+                var startToken = this._getTokenCount(structure.slice(0, matchStart)) - 1;
+                var endToken = startToken + this._getTokenCount(match[0]) - 1;
+                var endCharIndex = matchStart;
+                _.each(tokens.slice(startToken, endToken), function (token) {
+                    endCharIndex += token.text.length;
+                });
+
+                var newSentence = '';
+                _.each(tokens.slice(0, startToken), function(token) {
+                    newSentence += token.text;
+                });
+                _.each(modifierRule.newStructure.split(','), function (structureMatchGroup) {
+                    var tokenOffset = startToken;
+                    structureMatchGroup = parseInt(structureMatchGroup.slice(1));
+                    var groupCount = 1;
+                    while (groupCount < structureMatchGroup) {
+                        tokenOffset += this._getTokenCount(match[groupCount]); 
+                        groupCount += 1;
+                    }
+                    _.each(tokens.slice(tokenOffset, tokenOffset + this._getTokenCount(match[structureMatchGroup])), function (token) {
+                        newSentence += token.text;
+                    });
+                }, this);
+                var editItem = new EditItem(matchStart, endCharIndex, newSentence, 'misplaced-modifier');
             }
             return isMatchingStructure;
-        });
+        }, this);
         return isMatchingStructure;
+    },
+
+    /**
+     * Get the number of tokens represented by a string of part-of-speech tags
+     * @param {string} structure
+     * @return {number}
+     * @example _getTokenCount('IN,JJ,NN') => 3
+     */
+    _getTokenCount : function (structure) {
+        return structure.split(',').length;
     }
 });
 
